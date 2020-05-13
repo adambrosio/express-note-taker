05ea831ca462c624e82d329e0f16a51d01f70465
Andrew D'Ambrosio <adambrosio20@gmail.com>
2020-05-13 19:08:05 -0400

    route js and server created

--- Develop/server.js
+++ Develop/server.js
@@ -1,19 +1,6 @@
-var express = require("express");
-var path = require("path");
-let app = express();
-var PORT = 3000;
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/notes.html"));
-});
-
-app.get("/*", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/index.html"))
-});
-
-app.listen(PORT, function(){
-    console.log("Server is now listening on PORT " + PORT);
-});
