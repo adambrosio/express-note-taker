74fcc8a155de3c2fcb8ebb3ca79780a9fd789130
Andrew D'Ambrosio <adambrosio20@gmail.com>
2020-05-14 18:10:06 -0400

    functioning server

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,16 @@
+// Requiring dependencies
 const express = require("express");
+// Create express server
 const app = express();
+// Set initial port
 const PORT = process.env.PORT || 3000;
-
+// Express app handling data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+// Point server to route files
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+app.listen(PORT, function(){
+    console.log("App is listening on PORT: " + PORT);
+});
